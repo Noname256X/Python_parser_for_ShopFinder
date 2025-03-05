6188fc52e3aecb7e9086a99cda9bd674f65fb1a3
Alexey Roslyakov <fodeasypfr@gmail.com>
2025-03-05 23:48:41 +0300

    first_commit parsingSites

---
+++ main.py
@@ -0,0 +1,171 @@
+import time
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def get_products_links_Ozon(item_name):
+    driver = uc.Chrome(version_main=133)
+    driver.implicitly_wait(10)
+
+    try:
+        driver.get('https://ozon.ru')
+        time.sleep(7)
+
+        find_input = driver.find_element(By.NAME, 'text')
+        find_input.clear()
+        find_input.send_keys(item_name)
+        time.sleep(2)
+        find_input.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        try:
+            find_links = driver.find_elements(By.CSS_SELECTOR, 'div.tile-root a.tile-clickable-element')
+            product_urls = [link.get_attribute("href") for link in find_links if link.get_attribute("href") is not None]
+
+        except Exception as e:
+            print(f'[!] Что-то пошло не так при сборе ссылок на товары Ozon: {e}')
+
+        product_urls_dict = {}
+
+        for k, v in enumerate(product_urls):
+            product_urls_dict.update({k: v})
+
+        if product_urls:
+            with open('products_urls_ozon.json', 'w', encoding='utf-8') as file:
+                json.dump(product_urls, file, indent=4, ensure_ascii=False)
+            print(f'[+] Ссылки на товары Ozon сохранены в файл!')
+        else:
+            print('[!] Не удалось собрать ссылки на товары Ozon.')
+
+
+    finally:
+        driver.quit()
+
+
+def get_products_links_WB(item_name):
+    driver = uc.Chrome(version_main=133)
+    driver.implicitly_wait(10)
+
+    try:
+        driver.get('https://www.wildberries.ru')
+        time.sleep(7)
+
+        find_input = driver.find_element(By.CSS_SELECTOR, '[data-wba-header-name="Search_text"]')
+        find_input.clear()
+        find_input.send_keys(item_name)
+        time.sleep(2)
+        find_input.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        try:
+            find_links = driver.find_elements(By.CSS_SELECTOR, 'a.product-card__link')
+            product_urls = [link.get_attribute("href") for link in find_links if link.get_attribute("href") is not None]
+
+        except Exception as e:
+            print(f'[!] Что-то пошло не так при сборе ссылок на товары WB: {e}')
+
+        product_urls_dict = {}
+
+        for k, v in enumerate(product_urls):
+            product_urls_dict.update({k: v})
+
+        if product_urls:
+            with open('products_urls_wb.json', 'w', encoding='utf-8') as file:
+                json.dump(product_urls, file, indent=4, ensure_ascii=False)
+            print(f'[+] Ссылки на товары WB сохранены в файл!')
+        else:
+            print('[!] Не удалось собрать ссылки на товары WB.')
+
+    finally:
+        driver.quit()
+
+
+def get_products_links_YandexMarket(item_name):
+    driver = uc.Chrome(version_main=133)
+    driver.implicitly_wait(10)
+
+    try:
+        driver.get('https://market.yandex.ru')
+        time.sleep(7)
+
+
+        find_input = driver.find_element(By.NAME, 'text')
+        find_input.clear()
+        find_input.send_keys(item_name)
+        time.sleep(2)
+        find_input.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        try:
+            find_links = driver.find_elements(By.CSS_SELECTOR, 'a.EQlfk')
+            product_urls = {
+                link.get_attribute("href") for link in find_links
+                if link.get_attribute("href") is not None and "/product--" in link.get_attribute("href")
+            }
+
+        except Exception as e:
+            print(f'[!] Что-то пошло не так при сборе ссылок на товары Я.Маркет: {e}')
+
+        product_urls = list(product_urls)
+
+        if product_urls:
+            with open('products_urls_yandex_market.json', 'w', encoding='utf-8') as file:
+                json.dump(product_urls, file, indent=4, ensure_ascii=False)
+            print(f'[+] Ссылки на товары Я.Маркет сохранены в файл!')
+        else:
+            print('[!] Не удалось собрать ссылки на товары Я.Маркет.')
+
+    finally:
+        driver.quit()
+
+
+def get_products_links_MagnitMarket(item_name):
+    driver = uc.Chrome(version_main=133)
+    driver.implicitly_wait(10)
+
+    try:
+        driver.get('https://mm.ru')
+        time.sleep(7)
+
+
+        find_input = driver.find_element(By.CSS_SELECTOR, 'input.default-input')
+        find_input.clear()
+        find_input.send_keys(item_name)
+        time.sleep(2)
+        find_input.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        try:
+            find_links = driver.find_elements(By.CSS_SELECTOR, 'div[data-test-id="item__product-card"] a[ui-link="ui-link"]')
+            product_urls = [link.get_attribute("href") for link in find_links if link.get_attribute("href") is not None]
+
+        except Exception as e:
+            print(f'[!] Что-то пошло не так при сборе ссылок на товары М.Маркет: {e}')
+
+        product_urls = list(product_urls)
+
+        if product_urls:
+            with open('products_urls_magnit_market.json', 'w', encoding='utf-8') as file:
+                json.dump(product_urls, file, indent=4, ensure_ascii=False)
+            print(f'[+] Ссылки на товары М.Маркет сохранены в файл!')
+        else:
+            print('[!] Не удалось собрать ссылки на товары М.Маркет.')
+
+    finally:
+        driver.quit()
+
+
+def main():
+    get_products_links_Ozon('наушники xiaomi')
+    get_products_links_WB('наушники xiaomi')
+    get_products_links_YandexMarket('наушники xiaomi')
+    get_products_links_MagnitMarket('наушники xiaomi')
+
+
+
+if __name__ == '__main__':
+    main()
